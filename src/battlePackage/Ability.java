a6d20148f49bcfb8a59de9da67a39e03703a9b5f
natanfudge <natan.lifsiz@gmail.com>
2017-03-05 13:43:12 +0200

    reformatting

---
+++ src/battlePackage/Ability.java
@@ -0,0 +1,8 @@
+package battlePackage;
+
+/**
+ * Created by natan on 3/5/2017.
+ */
+public enum Ability {
+    normal,magicMissle,ranged,heal;
+}
--- src/battlePackage/Player.java
+++ src/battlePackage/Player.java
@@ -12,109 +12,104 @@ public abstract class Player implements Character {
 	private PlayerType playerType;
 	private float attackSpeed;
 	private float attackDelay;
+	private Ability ability1;
+    private Ability ability2;
+    private Ability ability3;
+    private Ability ability4;
+    private Ability ability5;
+    private Ability ability6;
 
 	public int getHealth() {
 		return health;
-	}
-
-	public void setHealth(int health) {
+	}   public void setHealth(int health) {
 		this.health = health;
 	}
 
 	public int getMana() {
 		return mana;
-	}
-
-	public void setMana(int mana) {
+	}   public void setMana(int mana) {
 		this.mana = mana;
 	}
 
 	public int getAttackDamage() {
 		return attackDamage;
-	}
-
-	public void setAttackDamage(int attackDamage) {
+	}   public void setAttackDamage(int attackDamage) {
 		this.attackDamage = attackDamage;
 	}
 
 	public int getSpellDamage() {
 		return spellDamage;
-	}
-
-	public void setSpellDamage(int spellDamage) {
+	}   public void setSpellDamage(int spellDamage) {
 		this.spellDamage = spellDamage;
 	}
 
-	public float getHealthRegeneration() {
-		return healthRegeneration;
-	}
+	public float getHealthRegeneration() {return healthRegeneration;}public void setHealthRegeneration(float healthRegeneration) {this.healthRegeneration = healthRegeneration;}
 
-	public void setHealthRegeneration(float healthRegeneration) {
-		this.healthRegeneration = healthRegeneration;
-	}
-
-	public float getManaRegeneration() {
-		return manaRegeneration;
-	}
-
-	public void setManaRegeneration(float manaRegeneration) {
-		this.manaRegeneration = manaRegeneration;
-	}
+	public float getManaRegeneration() {return manaRegeneration;}  public void setManaRegeneration(float manaRegeneration) {this.manaRegeneration = manaRegeneration;}
 
 	public String getName() {
 		return name;
-	}
-
-	public void setName(String name) {
+	}   public void setName(String name) {
 		this.name = name;
 	}
 
 	public PlayerType getPlayerType() {
 		return playerType;
-	}
-
-	public void setPlayerType(PlayerType playerType) {
+	}   public void setPlayerType(PlayerType playerType) {
 		this.playerType = playerType;
-	}
-
-	public Player(int health, int mana, int attackDamage,float attackSpeed,int spellDamage, String name) {
-		super();
-		this.health = health;
-		this.mana = mana;
-		this.attackDamage = attackDamage;
-		this.name = name;
-		this.attackSpeed = attackSpeed;
-		attackDelay= 1 / attackSpeed;
-		this.spellDamage=spellDamage;
-
-	}
-
-	@Override
-	public void attack(Character character) {
-		character.attacked(getAttackDamage());
-	}
-
-	@Override
-	public void attacked(int afterMathEffect) {
-		setHealth(getHealth() - afterMathEffect);
-		;
 	}
 
 	public float getAttackDelay() {
 		return attackDelay;
-	}
-
-	public void setAttackDelay(float attackDelay) {
+	}   public void setAttackDelay(float attackDelay) {
 		this.attackDelay = attackDelay;
 	}
 
-	public void setAttackSpeed(float attackSpeed) {
+    public float getAttackSpeed() {
+        return attackSpeed;
+    }   public void setAttackSpeed(float attackSpeed) {
 		this.attackSpeed = attackSpeed;
 	}
 
-	public float getAttackSpeed() {
-		return attackSpeed;
-	}
+    public Ability getAbility1() {return ability1;} public void setAbility1(Ability ability1) {this.ability1 = ability1;}
+
+    public Ability getAbility2() {return ability2;} public void setAbility2(Ability ability2) {this.ability2 = ability2;}
+
+    public Ability getAbility3() {return ability3;} public void setAbility3(Ability ability3) {this.ability3 = ability3;}
+
+    public Ability getAbility4() {return ability4;} public void setAbility4(Ability ability4) {this.ability4 = ability4;}
+
+    public Ability getAbility5() {return ability5;} public void setAbility5(Ability ability5) {this.ability5 = ability5;}
+
+    public Ability getAbility6() {return ability6;} public void setAbility6(Ability ability6) {this.ability6 = ability6;
+    }
+
+
+
+
+    public Player(int health, int mana, int attackDamage,float attackSpeed,int spellDamage, String name) {
+        super();
+        this.health = health;
+        this.mana = mana;
+        this.attackDamage = attackDamage;
+        this.name = name;
+        this.attackSpeed = attackSpeed;
+        attackDelay= 1 / attackSpeed;
+        this.spellDamage=spellDamage;
+
+    }
+
+
+    @Override
+    public void attack(Character character) {
+        character.attacked(getAttackDamage());
+    }
+
+    @Override
+    public void attacked(int afterMathEffect) {
+        setHealth(getHealth() - afterMathEffect);
+        ;
+    }
 }
 
 	
