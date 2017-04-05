57a067bbbf86e33efa77286cd2eeb88c8e26f891
natanfudge <natan.lifsiz@gmail.com>
2017-04-05 19:19:37 +0300

    Beans

--- src/battlePackage/Ability.java
+++ src/battlePackage/Ability.java
@@ -8,25 +8,84 @@ import java.util.Properties;
 import java.util.StringTokenizer;
 
 public class Ability {
+
     private static Properties prop=Configurable.loadConfig("abilities");
 
 
    public static Float getDamage(String abilityName, Character character){
+       Float output = null;
+       String action=null;
        String abilityProp = prop.getProperty(abilityName);
+       StringTokenizer tokenizer=new StringTokenizer(abilityProp,"+-*/~",true);
 
-       abilityProp=abilityProp.substring(0,1).toUpperCase()+abilityProp.substring(1);
-       abilityProp="T"+abilityProp;
-Float o = null;
-       try {
-         o=  Float.valueOf(BeanUtils.getProperty(character,abilityProp));
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
-       StringTokenizer tokenizer=new StringTokenizer(abilityProp,"+-*/",true);
-while(tokenizer.hasMoreElements()){
-  Utils.p(""+tokenizer.nextElement()+" ");
+       while(tokenizer.hasMoreTokens()){
+        String currentInput=tokenizer.nextToken();
+
+    if(action=="/"||action=="*"||action=="+"||action=="-") {
+
+        float number=Float.valueOf(currentInput);
+        switch(action){
+            case "/":
+                output/=number;
+                action=null;
+
+                break;
+            case "*":
+                output*=number;
+                action=null;
+                break;
+            case "+":
+                output+=number;
+                action=null;
+                break;
+            case"-":
+                output-=number;
+                action=null;
+                break;
+
+        }
+   }else {
+        switch(currentInput){
+
+            case "/":
+                action = "/";
+                break;
+            case "*":
+                action = "*";
+                break;
+            case "+":
+                action = "+";
+                break;
+            case "-":
+                action = "-";
+                break;
+            case "~":
+                action = "~";
+                break;
+
+
+            default:
+                currentInput = currentInput.substring(0, 1).toUpperCase() + currentInput.substring(1);
+                currentInput = "T" + currentInput;
+                try {
+                    output = Float.valueOf(BeanUtils.getProperty(character, currentInput));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+
+
+        }
+    }
+
 }
-       return o;
+           Utils.p(""+output);
+
+
+
+
+
+       return output;
    }
 
 }
